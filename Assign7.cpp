7278a8af65c0520049d6a0bbbd94cd25653563a7
mayuejan <ray.mar@icloud.com>
2024-07-30 12:12:28 -0400

    Update Assign7.cpp

--- Assign7.cpp
+++ Assign7.cpp
@@ -1,20 +1,5 @@
 /**
 * File Name: Assign7.cpp
-* Academic Integrity Statement: 
-* I certify that, while others may have assisted me in brainstorming, debugging, and validating this program, 
-* the program itself is my own work. I understand that submitting code that is the work of other individuals 
-* is a violation of the course Academic Integrity Policy and may result in zero credit for the assignment, or 
-* course failure and a report to the Academic Dishonesty Board. I also understand that if I knowingly give my 
-* original work to another individual could also result in zero credit for the assignment, or course failure 
-* and a report to the Academic Dishonesty Board. See Academic Integrity for Students
-Links to an external site.
-* 
-* Assisted by and Assisted line numbers:Assisted by "https://www.youtube.com/watch?v=GBST5uQ_yos&t=735s" to gain
-* understanding of stack concept,Assisted line (28 ).Assisted by "https://www.youtube.com/watch?v=EaHFhms_Shw&t=249s"
-* because im a bit forgot file handleing, assisted line (83 ).
-* Your Name: YUERAN MA
-* Your PSU user ID:  ymm5252
-* Course title: CMPSC122 Summer 2024
 * Description: In this project,I implemented STL stack and was able to solve more realistic issues.I firstly declared a 
 * bool function to tell whether the need can be meet, this fucntion later bocome the most important part of this project.
 * at main function, I spent a lot of time on the first value received becasue it decides the total size of this array,
